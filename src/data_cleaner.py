11cfb7a98005816dc42644a7e83646806220bae6
Ravi <ravikumar6f@gmail.com>
2024-11-29 10:27:45 +0530

    completed with data ingestion and cleaning.

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from typing import Union
 from abc import ABC, abstractmethod
-
+from sklearn.model_selection import train_test_split
 
 class DataStrategy(ABC):
 
@@ -41,3 +41,38 @@ class DataPreprocessingStrat(DataStrategy):
             logging.error(f"{"Error in preprocessing the data {e}"}")
             raise e
 
+class DataSplitStrategy(DataStrategy):
+    def handle_data(self, data:pd.DataFrame)-> Union[pd.DataFrame, pd.Series]:
+        """
+        To do the train test split.
+        """
+        try:
+            x =  data.drop(columns=['review_score'])
+            y = data['review_score']
+            x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+            return x_train,x_test,y_train,y_test
+        except Exception as e:
+            logging.error(f"Error in train test split {e}")
+            raise e
+
+class DataCleaning:
+    """
+    This will utilize the classes above to process the data and make the train test split.
+    """
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy):
+        self.data = data
+        self.strategy = strategy
+    
+    def handle_data(self):
+        try:
+            return self.strategy.handle_data(self.data)
+        except Exception as e:
+            logging.error(f"error in handling the data {e}")
+            raise e
+    
+if __name__ == "__main__":
+    pass
+    # data = pd.read_csv(r"D:\ML_OPS_ZENML\data\olist_customers_dataset.csv")
+    # data_cleaning = DataCleaning(data, DataPreprocessingStrat())
+    # data_cleaning.handle_data()
+    # the above lines can help me call any pre processing strategy defined above.
--- steps/preprocess.py
+++ steps/preprocess.py
@@ -2,8 +2,33 @@ import pandas as pd
 import numpy as np
 import logging
 from zenml import step
+from src.data_cleaner import DataCleaning, DataPreprocessingStrat,DataSplitStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
-def pre_process(df: pd.DataFrame) ->None:
-    pass
+def pre_process(df: pd.DataFrame) ->Tuple[
+    Annotated[pd.DataFrame,"x_train"],
+    Annotated[pd.DataFrame,"x_test"],
+    Annotated[pd.DataFrame,"y_train"],
+    Annotated[pd.DataFrame,"y_test"],
+]:
+    """
+    Args : data frame.
+    return : data split as train test.
+    """
+    try:
+        pre_process_strat = DataPreprocessingStrat()
+        pre_prcessing = DataCleaning(df,pre_process_strat)
+        clean_data = pre_prcessing.handle_data()
+
+        divide_strat =  DataSplitStrategy()
+        splitting = DataCleaning(clean_data, DataSplitStrategy())
+        x_train, x_test,y_train,y_test =  splitting.handle()
+        #return x_train, x_test,y_train,y_test
+        logging.info("Data cleaning and splitting complete")
+    except Exception as e:
+        logging.error(f"error occoured while performing pipeline step tp clean{e}")
+        raise e
+
 
